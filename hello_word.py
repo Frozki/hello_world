3953985410a305fb9afdc3c76c5ac18dd3d73bc3
Frozki <53174763+Frozki@users.noreply.github.com>
2019-07-22 18:08:48 +0800

    Add files via upload

---
+++ hello_word.py
@@ -0,0 +1,18 @@
+print("hello world")
+print("hello LuoTie!")
+print("hello python!")
+ 
+message="Hello Python world!"
+print(message)
+
+message="Hello Python Crash Course world!"
+print(message)
+
+A="Wow,that's cool"
+print(A)
+
+message="Hello,nice to meet you!"
+print(message)
+
+A='I told my friend,"Python is my favorite language!"'
+print(A)
